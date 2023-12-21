16f1fc079631ae578d38739a4770b2618ae46398
hola-aryan <aryanisastudent@gmail.com>
2023-12-21 22:24:09 +0530

    Expense tracker front end before aws

---
+++ front_end/script.js
@@ -0,0 +1,41 @@
+var naam = document.getElementById('name')
+var email = document.getElementById('email')
+var password = document.getElementById('password')
+var form = document.getElementById('form')
+
+form.addEventListener('submit', check);
+
+function check(e) {
+    e.preventDefault();
+  
+    // Get user input values
+    const names = naam.value;
+    const emails = email.value;
+    const passwords= password.value;
+  
+  
+    // Simple client-side validation
+    if (!names || !emails || !passwords) {
+      alert('Please fill in all fields');
+      return;
+    }
+  
+    axios.post('http://localhost:2500/signIn', {
+        name : names,
+        email : emails,
+        password : passwords 
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+        alert('Error submitting data. Please try again.');
+      })
+      .finally(() => {
+        // Reset form fields after request (whether success or failure)
+        compName.value = "";
+        cons.value = "";
+        pros.value = "";
+      });
+  }
